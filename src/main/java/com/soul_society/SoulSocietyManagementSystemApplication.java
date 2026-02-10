52e8f7b67bd3ddb37aaf4e6907a924d40b5861be
reynanwq <reynanwq@gmail.com>
2026-02-10 11:12:51 -0300

    Initial commit

---
+++ src/main/java/com/soul_society/SoulSocietyManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.soul_society;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulSocietyManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulSocietyManagementSystemApplication.class, args);
+	}
+
+}
